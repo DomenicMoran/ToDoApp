7359013387368c32b47280d0246d4930c38fb5ee
Domenic <domenicmoran@gmail.com>
2022-08-17 14:51:35 +0200

    Anfang

---
+++ todoApp.swift
@@ -0,0 +1,17 @@
+//
+//  todoApp.swift
+//  todo
+//
+//  Created by Domenic Moran on 17.08.22.
+//
+
+import SwiftUI
+
+@main
+struct todoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
